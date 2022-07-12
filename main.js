29dfd7d4dac21942cebe3e52e686d2b1c7d76211
Hetoreno <hectortamriel@gmail.com>
2022-07-11 22:13:50 -0700

    added work experience section for portfolio

--- main.js
+++ main.js
@@ -13,3 +13,27 @@ window.addEventListener("scroll",()=>{
       mobileNav.classList.remove("active");
     }
 })
+
+//Work Experience modal
+const learnMoreBtn = document.querySelectorAll(".modalBtn");
+
+learnMoreBtn.forEach(event =>{
+  event.addEventListener("click", ()=>{
+
+    //displaying modals
+    const modal = event.nextElementSibling;
+    modal.classList.add("show");
+    const shown = document.querySelector(".show");
+
+    shown.style.transition = "all 0.4s";
+    
+  })
+})
+
+//closing modals
+const closeBtn = document.querySelectorAll(".close");
+    closeBtn.forEach(closing=>{
+      closing.addEventListener("click",()=>{
+        closing.closest(".modalContainer").classList.remove("show");
+      })
+    })
